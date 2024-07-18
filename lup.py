37c22ee04a0c524c1825f6fa27ecb7164ea3d984
ss0832 <80376408+ss0832@users.noreply.github.com>
2024-07-18 22:03:17 +0900

    Add files via upload

---
+++ lup.py
@@ -0,0 +1,67 @@
+import numpy as np
+import mb_pot
+import derivatives
+
+# ref.1: https://superadditive.com/notes/cecam-molecular-kinetics-2016/locally-updated-planes.html
+# ref.2: Ulitsky, A., & Elber, R. (1990). A new technique to calculate steepest descent paths in flexible polyatomic systems. The Journal of Chemical Physics, 92(2), 1510. doi:10.1063/1.458112
+# ref.3: Müller, K. (1980). Reaction paths on multidimensional energy hypersurfaces. Angewandte Chemie International Edition in English, 19(1), 1–13. doi:10.1002/anie.198000013
+ 
+def lst(init_point_1, init_point_2, n_points):# for initialization of the path
+    #linear synchronous transit method
+    #ref.: T.A. Halgren, W. N. Lipscomb, Chem. Phys. Lett. 49, 225 (1977)
+    point_list = []
+    for i in range(n_points + 1):
+        point_list.append(init_point_1 + i * (init_point_2 - init_point_1) / n_points)
+    point_list.append(init_point_2)
+    point_list = np.array(point_list)
+    return point_list # length:n_points + 2
+
+def calc_tangent_vec(point1, point2):
+    return (point2 - point1) / np.linalg.norm(point2 - point1)
+
+def project_tangent_vec_for_grad(grad, tangent):
+    proj_grad = grad - (grad * tangent) * tangent
+    return proj_grad
+
+def project_tangent_vec_for_hess(hess, tangent):
+    ones_mat = np.eye(2)
+    L_mat = tangent
+    E_LL = ones_mat -1* np.dot(L_mat, L_mat.T)
+    
+    hess_proj = np.dot(np.dot(E_LL, hess), E_LL)
+    return hess_proj
+
+
+def lup(point_list, alpha=3e-4):
+    new_point_list = []
+    new_point_energy_list = []
+    
+    for i in range(len(point_list)):
+        point = point_list[i]
+        print("# STEP", i)
+        if i == 0 or i == len(point_list)-1:
+            grad = derivatives.grad(mb_pot.muller_brown_potential, point[0][0], point[1][0])
+            
+            new_point = point - alpha * grad
+           
+            new_point_list.append(new_point)
+            new_point_energy_list.append(mb_pot.muller_brown_potential(new_point[0][0], new_point[1][0]))
+        else:
+            grad = derivatives.grad(mb_pot.muller_brown_potential, point[0][0], point[1][0])
+            hess = derivatives.hess(mb_pot.muller_brown_potential, point[0][0], point[1][0])
+            tangent = calc_tangent_vec(point_list[i-1], point_list[i+1])
+            proj_grad = project_tangent_vec_for_grad(grad, tangent)
+            proj_hess = project_tangent_vec_for_hess(hess, tangent)
+            move_vec = -1 * np.dot(np.linalg.pinv(proj_hess), proj_grad)
+            
+            trust_radius = min(np.linalg.norm(point - point_list[i-1]), np.linalg.norm(point_list[i+1] - point)) / 2
+            move_vec = move_vec * min(trust_radius, np.linalg.norm(move_vec)) / np.linalg.norm(move_vec)
+            new_point = point + move_vec
+           
+            new_point_list.append(new_point)
+            new_point_energy_list.append(mb_pot.muller_brown_potential(new_point[0][0], new_point[1][0]))
+    new_point_list = np.array(new_point_list)
+    new_point_energy_list = np.array(new_point_energy_list)
+   
+    return new_point_list, new_point_energy_list
+    
---
+++ main.py
@@ -0,0 +1,60 @@
+import mb_pot
+import derivatives
+import lup
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+print("Locally Updated Plain (LUP) method for Muller-Brown potential")
+init_point = np.array([[0.823499], [0.028038]]) #initial points
+end_point = np.array([[0.158233], [0.501716]])
+iteration = 40
+number_of_node = 14
+
+init_point_list = lup.lst(init_point, end_point, n_points=number_of_node)
+
+# save Init condition
+level = []
+for i in range(-10, 40):
+    level.append(15*i)
+point_list = init_point_list
+plt.plot(point_list.T[0][0], point_list.T[0][1], 'w.--')
+x_list = np.linspace(-2.0, 1.5, 500)
+y_list = np.linspace(-1.0, 2.5, 500)
+plt.title('Iinitialization')
+plt.xlabel('x')
+plt.ylabel('y')
+x_mesh, y_mesh = np.meshgrid(x_list, y_list)
+f_mesh = mb_pot.muller_brown_potential(x_mesh, y_mesh)
+cont = plt.contourf(x_mesh, y_mesh, f_mesh, levels=level, cmap='jet')
+plt.colorbar()
+plt.savefig('lup_init.png')
+plt.close()
+# -----
+
+history = []
+
+for j in range(iteration):
+    point_list, energy_list = lup.lup(point_list)
+    history.append([point_list, energy_list])
+    print("Iteration: ", j)
+    level = []
+    for i in range(-10, 40):
+        level.append(15*i)
+
+    
+    plt.plot(point_list.T[0][0], point_list.T[0][1], 'w.--')
+    x_list = np.linspace(-2.0, 1.5, 500)
+    y_list = np.linspace(-1.0, 2.5, 500)
+    plt.title('Iteration: {}'.format(j))
+    plt.xlabel('x')
+    plt.ylabel('y')
+    x_mesh, y_mesh = np.meshgrid(x_list, y_list)
+    f_mesh = mb_pot.muller_brown_potential(x_mesh, y_mesh)
+    cont = plt.contourf(x_mesh, y_mesh, f_mesh, levels=level, cmap='jet')
+    plt.colorbar()
+    plt.savefig('lup_{}.png'.format(j))
+    plt.close()
+
+print("LUP finished.")
